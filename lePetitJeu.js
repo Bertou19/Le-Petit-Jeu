29ee8825f636fca03b3efb62227cb6a3cdd32276
Bertille De Almeida <a_bertille@msn.com>
2022-07-01 09:51:57 +0200

    debogage( non ajout du roundScore au globalScore au click du bouton "hold")

--- lePetitJeu.js
+++ lePetitJeu.js
@@ -9,7 +9,7 @@ tour de l’autre joueur.
 - Lancer le dé. S’il obtient un 1, son score ROUND est perdu et c’est la fin de son tour.
 Le premier joueur qui atteint les 100 points sur global gagne le jeu.*/
 
-let gamePlay ;let globalScore ;let roundScore ; let player;var dice
+let gamePlay ;let globalScore ;let roundScore ; let player;var dice; 
 
 restart();
 
@@ -19,51 +19,60 @@ document.getElementById("roll").addEventListener('click',function(){
   if (gamePlay){
     dice = Math.floor(Math.random()*6)+1;
   
-//Afficher le resultat
-let diceDOM= document.querySelector('.dice');
-diceDOM.style.display = 'block';
-diceDOM.src = 'face-'+ dice + '.png';
+    //Afficher le resultat
+    let diceDOM= document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'face-'+ dice + '.png';
+    console.log("valeur du dé " + dice) // // débogage de valeur de 'dice'
 
-//Ajout du score à roundScore si le dé ne tombe pas sur 1
-if (dice !==1){
-  roundScore+=dice;
-
+    //Ajout du score à roundScore si le dé ne tombe pas sur 1
+    if (dice !==1){
+      roundScore+=dice;
+      console.log("Num player : " + player) // débogage de valeur de 'player'
+      console.log("valeur du roundScore : "+  roundScore) // débogage de valeur 'roundScore'
   
 
-document.querySelector('#roundScore-'+ player).textContent = roundScore;
-}else{
-  nextPlayer();
-}
-}
+      document.querySelector('#roundScore-'+ player).textContent = roundScore;
+    }else{
+      
+      document.querySelector('#roundScore-'+player).textContent = 0;
+      nextPlayer();
+    }   
+  }
 });
 
 
 //ajout d'un évènement sur le click du bouton 'HOLD'
 document.getElementById('hold').addEventListener('click',function(){
+  console.log("gamePlay"+ gamePlay)
   if(gamePlay){
-    globalScore[player]+=roundScore;//le roundScore du joueur actif s'ajoute au globalScore
+    console.log(globalScore[player-1])
+    
+    globalScore[player-1]+= roundScore//le roundScore du joueur actif s'ajoute au globalScore(position-1 pour tomber dsans la première case du tableau)
   
-  document.querySelector('#score-'+player).textContent = globalScore[player];
+    console.log(player)
+    console.log(globalScore[player-1])
+  
+    document.querySelector('#globalScore-'+player).innerHTML = globalScore[player-1];
+    
+    if (globalScore[player-1]>= 100){
 
-if (globalScore[player]>= 100){
-
-document.querySelector('#player-'+ player).textContent = 'Bravo ! Vous avez gagné !';
-document.querySelector('.dice').style.display ='none';
-document.querySelector('#player-'+player+'.side-').classList.add('winner')
-document.querySelector('#player-'+player + '.side-').classList.remove('active');
-gamePlay=false;
-}else{
-  nextPlayer();
-}
-}
-}
-);
+      document.querySelector('#player-'+ player).textContent = 'Bravo ! Vous avez gagné !';
+      document.querySelector('.dice').style.display ='none';
+      document.querySelector('#player-'+player+'.side-').classList.add('winner')
+      document.querySelector('#player-'+player + '.side-').classList.remove('active');
+      gamePlay=false;
+    }else{
+      nextPlayer();
+    }
+  }
+});
 
 function nextPlayer(){
-  player ===0 ? player = 1: player =0; roundScore =0;
+  player ===1 ? player = 2: player =1; roundScore =0;
 
-  document.getElementsById('roundScore-1').textContent = "0";
-  document.getElementsById('roundScore-2').textContent = "0";
+  document.getElementById('roundScore-1').textContent = "0";
+  document.getElementById('roundScore-2').textContent = "0";
 
   document.querySelector('.side-left').classList.toggle('active');
   document.querySelector('.side-right').classList.toggle('active');
@@ -72,34 +81,40 @@ function nextPlayer(){
   
   document.querySelector('.dice').style.display= 'none';
 }
+
 //reinitialisation de la partie
 document.getElementById("new").addEventListener('click',restart);
 
 function restart(){
-roundScore = [0,0];
-globalScore =  0;
-player = 0;
-gamePlay = true;
-console.log("le score est remis à 0")
+  roundScore = 0;
+  globalScore =  [0,0];
+  player = 1;
+  gamePlay = true;
+  console.log("le score est remis à 0")
 
-document.querySelector('.dice').style.display ='none';
+  document.querySelector('.dice').style.display ='none';
 
-document.getElementById('score-1').textContent = '0';
-document.getElementById('score-2').textContent = '0';
+  document.getElementById('globalScore-1').textContent = '0';
+  document.getElementById('globalScore-2').textContent = '0';
 
-document.getElementById('roundScore-1').textContent = "0";
-document.getElementById('roundScore-2').textContent = "0";
+  document.getElementById('roundScore-1').textContent = "0";
+  document.getElementById('roundScore-2').textContent = "0";
 
-document.querySelector('.side-left').classList.remove('winner')
-document.querySelector('.side-right').classList.remove('winner')
-document.querySelector('.side-left').classList.remove('active')
-document.querySelector('.side-right').classList.remove('active')
-document.querySelector('.side-left').classList.add('active');
+  document.querySelector('.side-left').classList.remove('winner')
+  document.querySelector('.side-right').classList.remove('winner')
+  document.querySelector('.side-left').classList.remove('active')
+  document.querySelector('.side-right').classList.remove('active')
+  document.querySelector('.side-left').classList.add('active');
+  //debogage : 
+  console.log("Affichage des variables")
+  console.log("roundScore-1 : " + roundScore-1)
+  console.log("roundScore-2 : " + roundScore-2)
+  console.log("roundScore : "+ roundScore)
+ 
 }
 
 document.querySelector('#roundScore-'+ player).textContent = dice;
 document.querySelector('#roundScore-'+ player).innerHTML = '<span>'+ dice+ '</span>';
-let x = document.querySelector('#score-1').textContent;
 
 
 
@@ -107,4 +122,19 @@ let x = document.querySelector('#score-1').textContent;
 
 
 
-//A chaque tour, le joueur a son round initialisé à 0
+ 
+
+
+
+
+ 
+
+
+
+
+  
+
+
+
+
+
